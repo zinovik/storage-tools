8a862f55a39f4ceca314153babe7c05ebf9bf195
Max Zinovik <zinovik@gmail.com>
2024-03-08 19:01:01 +0100

    add upload script

--- upload.js
+++ upload.js
@@ -3,6 +3,14 @@ const { Storage, TransferManager } = require('@google-cloud/storage');
 
 const DIRECTORY = 'photos';
 const BUCKET_NAME = 'hedgehogs';
+const FILES_TO_SAVE = [
+    'hedgehogs.json',
+    'albums.json',
+    'files.json',
+    'sources-config.json',
+    'digital-board-games.json',
+];
+const FOLDERS_TO_SAVE = ['photos', 'zanzibar', 'test-folder'];
 
 const localFilenames = fs.readdirSync(DIRECTORY);
 
@@ -11,7 +19,61 @@ const bucket = storage.bucket(BUCKET_NAME);
 
 const transferManager = new TransferManager(bucket);
 
+const removeUnrelatedFiles = async (bucket, filesToSave, foldersToSave) => {
+    const [exitingFiles] = await bucket.getFiles({ versions: true });
+
+    const filesToRemove = exitingFiles
+        .filter(
+            (file) =>
+                !filesToSave.includes(file.name) &&
+                !foldersToSave.some((folder) =>
+                    file.name.startsWith(`${folder}/`)
+                )
+        )
+        .map((file) =>
+            bucket.file(file.name, {
+                generation: file.generation,
+            })
+        );
+
+    console.log(`Unrelated files to remove: ${filesToRemove.length}`);
+
+    await Promise.all(filesToRemove.map((file) => file.delete()));
+};
+
+const filterNotCurrentVersions = (files) => {
+    const currentVersionMap = {};
+
+    files.forEach((file) => {
+        if (
+            !currentVersionMap[file.name] ||
+            currentVersionMap[file.name] < file.generation
+        )
+            currentVersionMap[file.name] = file.generation;
+    });
+
+    return files.filter(
+        (file) => file.generation !== currentVersionMap[file.name]
+    );
+};
+
+const removeNotCurrentFiles = async (bucket) => {
+    const [exitingFiles] = await bucket.getFiles({ versions: true });
+
+    const filesToRemove = filterNotCurrentVersions(exitingFiles).map((file) =>
+        bucket.file(file.name, {
+            generation: file.generation,
+        })
+    );
+
+    console.log(`Not current files to remove: ${filesToRemove.length}`);
+
+    await Promise.all(filesToRemove.map((file) => file.delete()));
+};
+
 (async () => {
+    await removeUnrelatedFiles(bucket, FILES_TO_SAVE, FOLDERS_TO_SAVE);
+
     const [exitingFiles] = await bucket.getFiles();
     const exitingFilenames = exitingFiles.map(
         (exitingFile) => exitingFile.name
@@ -25,6 +87,11 @@ const transferManager = new TransferManager(bucket);
     await transferManager.uploadManyFiles(filesToUpload, {
         passthroughOptions: {
             public: true,
+            metadata: {
+                cacheControl: 'public, max-age=31536000',
+            },
         },
     });
+
+    await removeNotCurrentFiles(bucket);
 })();
