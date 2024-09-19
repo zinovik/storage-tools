c6acf2646240b11fc9651a301f5af8585d0266d1
Max Zinovik <zinovik@gmail.com>
2024-09-19 20:03:10 +0200

    accesses refactoring

--- src/update-storage-file-accesses.js
+++ src/update-storage-file-accesses.js
@@ -14,34 +14,37 @@ const getExitingStorageFilePaths = async (bucket) => {
     return exitingFiles.map((exitingFile) => exitingFile.name);
 };
 
-const getFileAccesses = async (bucket) => {
+const getStorageFilePathsIsPublic = async (bucket, storageFilePaths) => {
+    const results = [];
+
+    for (let i = 0; i < storageFilePaths.length; i += ACCESS_BATCH_SIZE) {
+        console.log(`- get file access batch starting from ${i}`);
+        const promises = storageFilePaths
+            .slice(i, i + ACCESS_BATCH_SIZE)
+            .map(async (storageFilePath) => ({
+                storageFilePath,
+                isPublic: await getIsPublic(bucket, storageFilePath),
+            }));
+
+        results.push(...(await Promise.all(promises)));
+    }
+
+    return results;
+};
+
+const getFilesFromJson = async (bucket) => {
     const file = await bucket.file(FILES_FILE_NAME).download();
 
-    return JSON.parse(file.toString()).map((file) => ({
-        filename: file.filename,
-        path: file.path,
-        accesses: file.accesses,
-    }));
+    return JSON.parse(file.toString());
 };
 
-const getAllAlbumAccesses = async (bucket) => {
+const getAlbumsFromJson = async (bucket) => {
     const file = await bucket.file(ALBUMS_FILE_NAME).download();
 
-    const albums = JSON.parse(file.toString());
-
-    return [
-        ...albums.filter(
-            (album) => album.accesses && album.accesses.length > 0
-        ),
-    ]
-        .sort((a1, a2) => a2.path.split('/').length - a1.path.split('/').length)
-        .map((album) => ({
-            path: album.path,
-            accesses: album.accesses,
-        }));
+    return JSON.parse(file.toString());
 };
 
-const getIsPublicCurrent = async (bucket, storageFilePath) => {
+const getIsPublic = async (bucket, storageFilePath) => {
     const file = bucket.file(storageFilePath);
     const [acl] = await file.acl.get();
 
@@ -50,51 +53,27 @@ const getIsPublicCurrent = async (bucket, storageFilePath) => {
     );
 };
 
-// TODO: Read all and then update all
-const updateFileAccesses = async (
-    bucket,
-    storageFilePaths,
-    publicFilenames
+const getAccessesToUpdate = (
+    storageFilePathsIsPublic,
+    filesFromJson,
+    albumsFromJson
 ) => {
-    for (let i = 0; i < storageFilePaths.length; i += ACCESS_BATCH_SIZE) {
-        console.log(`- update file access batch starting from ${i}`);
-        const promises = storageFilePaths
-            .slice(i, i + ACCESS_BATCH_SIZE)
-            .map(async (storageFilePath) => {
-                const isPublicCurrent = await getIsPublicCurrent(
-                    bucket,
-                    storageFilePath
-                );
+    const albumAccesses = [
+        ...albumsFromJson.filter(
+            (album) => album.accesses && album.accesses.length > 0
+        ),
+    ]
+        .sort((a1, a2) => a2.path.split('/').length - a1.path.split('/').length)
+        .map((album) => ({
+            path: album.path,
+            accesses: album.accesses,
+        }));
 
-                if (
-                    isPublicCurrent &&
-                    !publicFilenames.includes(getFilename(storageFilePath))
-                ) {
-                    console.log(`Make PRIVATE: ${storageFilePath}`);
-                    await bucket
-                        .file(storageFilePath)
-                        .makePrivate({ strict: true });
-                }
-
-                if (
-                    !isPublicCurrent &&
-                    publicFilenames.includes(getFilename(storageFilePath))
-                ) {
-                    console.log(`Make PUBLIC: ${storageFilePath}`);
-                    await bucket.file(storageFilePath).makePublic();
-                }
-            });
-
-        await Promise.all(promises);
-    }
-};
-
-const getPublicFilenames = (accessesFromJson, allAlbumAccesses) =>
-    accessesFromJson
+    const publicFilenamesFromJson = filesFromJson
         .filter((file) =>
             (
                 file.accesses ||
-                allAlbumAccesses.find((albumAccess) =>
+                albumAccesses.find((albumAccess) =>
                     albumAccess.path.includes(file.path.split('/')[0])
                 )?.accesses ||
                 []
@@ -102,23 +81,85 @@ const getPublicFilenames = (accessesFromJson, allAlbumAccesses) =>
         )
         .map((file) => file.filename);
 
+    const makePublicPaths = [];
+    const makePrivatePaths = [];
+
+    storageFilePathsIsPublic.forEach(({ isPublic, storageFilePath }) => {
+        if (
+            !isPublic &&
+            publicFilenamesFromJson.includes(getFilename(storageFilePath))
+        )
+            makePublicPaths.push(storageFilePath);
+
+        if (
+            isPublic &&
+            !publicFilenamesFromJson.includes(getFilename(storageFilePath))
+        )
+            makePrivatePaths.push(storageFilePath);
+    });
+
+    return [makePublicPaths, makePrivatePaths];
+};
+
+const updateStorageFileAccesses = async (
+    bucket,
+    storageFilePaths,
+    isPublic
+) => {
+    for (let i = 0; i < storageFilePaths.length; i += ACCESS_BATCH_SIZE) {
+        console.log(
+            `- update ${
+                isPublic ? 'public' : 'private'
+            } file access batch starting from ${i}`
+        );
+        const promises = storageFilePaths
+            .slice(i, i + ACCESS_BATCH_SIZE)
+            .map(async (storageFilePath) => {
+                if (isPublic) {
+                    console.log(`Make PUBLIC: ${storageFilePath}`);
+                    await bucket.file(storageFilePath).makePublic();
+                } else {
+                    console.log(`Make PRIVATE: ${storageFilePath}`);
+                    await bucket
+                        .file(storageFilePath)
+                        .makePrivate({ strict: true });
+                }
+            });
+
+        await Promise.all(promises);
+    }
+};
+
 (async () => {
     const storage = new Storage();
     const bucket = storage.bucket(BUCKET_NAME);
 
-    console.log('Get exiting file paths...');
+    console.log('Get exiting storage files...');
     const exitingStorageFilePaths = await getExitingStorageFilePaths(bucket);
 
-    console.log('Get file accesses...');
-    const accessesFromJson = await getFileAccesses(bucket);
-
-    console.log('Get album accesses...');
-    const allAlbumAccesses = await getAllAlbumAccesses(bucket);
-
-    const publicFilenames = getPublicFilenames(
-        accessesFromJson,
-        allAlbumAccesses
+    console.log('Get storage files accesses...');
+    const storageFilePathsIsPublic = await getStorageFilePathsIsPublic(
+        bucket,
+        exitingStorageFilePaths
     );
 
-    await updateFileAccesses(bucket, exitingStorageFilePaths, publicFilenames);
+    console.log('Get files...');
+    const filesFromJson = await getFilesFromJson(bucket);
+
+    console.log('Get albums...');
+    const albumsFromJson = await getAlbumsFromJson(bucket);
+
+    const [makePublicPaths, makePrivatePaths] = getAccessesToUpdate(
+        storageFilePathsIsPublic,
+        filesFromJson,
+        albumsFromJson
+    );
+
+    console.log(makePublicPaths, makePrivatePaths);
+
+    console.log('Update public accesses...');
+    await updateStorageFileAccesses(bucket, makePublicPaths, true);
+
+    console.log('Update private accesses...');
+    await updateStorageFileAccesses(bucket, makePrivatePaths, false);
 })();
