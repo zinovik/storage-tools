115a4cf224833324f05cb27d64ef33f35a6fef06
Max Zinovik <zinovik@gmail.com>
2025-07-22 19:50:18 +0200

    move hedgehogs to photos

--- src/3-upload-files-to-storage.js
+++ src/3-upload-files-to-storage.js
@@ -3,20 +3,10 @@ const { promisify } = require('util');
 const { Storage } = require('@google-cloud/storage');
 const { performBatch } = require('./perform-batch');
 
-const GALLERY_BUCKET_NAME = 'zinovik-gallery';
-const HEDGEHOGS_BUCKET_NAME = 'hedgehogs';
-
-// CHOOSE THE BUCKET
-const BUCKET_NAME = GALLERY_BUCKET_NAME;
-// const BUCKET_NAME = HEDGEHOGS_BUCKET_NAME;
-
-//
-
+const BUCKET_NAME = 'zinovik-gallery';
 const UPLOAD_BATCH_SIZE = 10;
 const FILES_FILE_NAME = 'files.json';
-const HEDGEHOGS_FILE_NAME = 'hedgehogs.json';
 const PHOTOS_PATH = '/home/max/photos';
-const HEDGEHOGS_BUCKET_FOLDER = 'photos';
 
 const SKIP_UPLOAD_PATHS = [];
 
@@ -40,18 +30,6 @@ const getFilenamesFromFile = async (bucket) => {
             )
         )
         .map((file) => file.filename);
-};
-
-const getFilenamesFromHedgehogs = async (bucket) => {
-    const file = await bucket.file(HEDGEHOGS_FILE_NAME).download();
-
-    return JSON.parse(file.toString()).reduce(
-        (acc, hedgehog) => [
-            ...acc,
-            ...hedgehog.photos.map((photoUrl) => getFilename(photoUrl)),
-        ],
-        []
-    );
 };
 
 const getExitingStorageFilePaths = async (bucket) => {
@@ -109,9 +87,7 @@ const mapFilenamesToLocalFilePaths = (filenames, localFilePaths) =>
     const bucket = storage.bucket(BUCKET_NAME);
 
     console.log('Get filenames from file...');
-    const filenamesFromJson = await (BUCKET_NAME === GALLERY_BUCKET_NAME
-        ? getFilenamesFromFile(bucket)
-        : getFilenamesFromHedgehogs(bucket));
+    const filenamesFromJson = await getFilenamesFromFile(bucket);
 
     console.log('Get exiting filenames...');
     const exitingStorageFilePaths = await getExitingStorageFilePaths(bucket);
@@ -128,10 +104,5 @@ const mapFilenamesToLocalFilePaths = (filenames, localFilePaths) =>
         allLocalFilePaths
     );
     console.log('Upload files...', localFilePathsToUpload);
-    await uploadFiles(
-        bucket,
-        localFilePathsToUpload,
-        BUCKET_NAME === GALLERY_BUCKET_NAME ? null : HEDGEHOGS_BUCKET_FOLDER,
-        BUCKET_NAME !== GALLERY_BUCKET_NAME
-    );
+    await uploadFiles(bucket, localFilePathsToUpload, null, false);
 })();
